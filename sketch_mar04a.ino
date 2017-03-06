4f84a7c45799d9eb6d884b83e79888f3fac807d0
q5dong <q5dong@ucsd.edu>
2017-03-05 17:05:21 -0800

    Add files via upload

--- sketch_mar04a.ino
+++ sketch_mar04a.ino
@@ -12,7 +12,7 @@ const int ledPin = A0;
 back to working, put the end of comment mark right after this line and you can see 
 how it works.
 
-***put the end of comment here***
+***put the end of comment here***/
 
 const int TOP_FLOOR = A1; //Set top floor to pin 13
 const int SECOND_FLOOR = A2; //Set the floor below top floor to pin 12
@@ -40,12 +40,50 @@ const int LEFT_GREEN = 2;
 const int LEFT_BLUE = 1;
 /*finish setting constants*/
 
+
 /*
  * Global setting: set global variables
- * 
+ * Arrays
  */
 
+ 
+/* Arrays: D1 array for floor operation; D2 array rows for position, 
+columns for specific color: Red is 0, Blue is 1, Green is 2 */
+int floors[4];
+int properties[4][3]; // the properties of led(at which position and which color)
+
 /*finish global setting*/
+
+/* Self created functions: wrap some basic transformations to do 
+further complicated ones. */
+
+/*Basic transformations*/
+
+/*
+ * void turnAllOff()
+ * turn all the leds on the cube off
+ */
+void turnAllOff(){
+  
+  // Perform turning off with each floor, start with the top floor;
+  for(int fl = 0; fl < 4 ;fl++}{
+    // sweep each floor by tracking position of every 4 led.
+    for(int pos = 0; pos < 4; pos++}{
+      // check and turn off every color
+      for(int color = 0; color < 3; color++){
+        
+      }
+    }
+  }
+}
+
+/*
+ * setColor(): 
+ * 
+ */
+void setColor(){}
+
+/* End of self created functions */
 
 void setup() {
   // put your setup code here, to run once:
@@ -84,6 +122,39 @@ void setup() {
   pinMode(LEFT_RED, OUTPUT);
   pinMode(LEFT_GREEN, OUTPUT);
   pinMode(LEFT_BLUE, OUTPUT);
+
+  // Assign arrays to corresponding pins
+  
+  /* Assigning floor arrays:
+  0 represents the top floor, 
+  3 represents the bottom floor */
+  floors[0] = TOP_FLOOR;
+  floors[1] = SECOND_FLOOR;
+  floors[2] = THIRD_FLOOR;
+  floors[3] = BOTTOM_FLOOR;
+  /* End assigning floors */
+
+  /* Assigning led properties:
+  for Rows:
+  0 for vertices, 1 for right, 2 for left, 3 for center.
+  for columns:
+  0 for red, 1 for green, 2 for blue */
+  properties[0][0] = VERTICES_RED;
+  properties[0][1] = VERTICES_GREEN;
+  properties[0][2] = VERTICES_BLUE;
+
+  properties[1][0] = RIGHT_RED;
+  properties[1][1] = RIGHT_GREEN;
+  properties[1][2] = RIGHT_BLUE;
+
+  properties[2][0] = LEFT_RED;
+  properties[2][1] = LEFT_GREEN;
+  properties[2][2] = LEFT_BLUE;
+
+  properties[3][0] = CENTER_RED;
+  properties[3][1] = CENTER_GREEN;
+  properties[3][2] = CENTER_BLUE;
+  /*End assigning properties*/
 }
 
 void loop() {
